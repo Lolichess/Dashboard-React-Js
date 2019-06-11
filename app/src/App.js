dea459d97f24a15b5d0ab7ce78c84ab1fd0318db
Lolichess <vjps1993@gmail.com>
2019-06-11 10:48:10 -0500

    Header-Begin

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Resources/css/App.css';
 import NavBar from './Component/NavBar';
+import Header from './Component/Header';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Header></Header>
     </div>
   );
 }
